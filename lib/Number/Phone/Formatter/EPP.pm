10b750a266a23e67feeb27d6452f760506ba16f1
Michael Schout <mschout@gkg.net>
2017-08-15 20:37:39 -0500

    make Number::Phone an optional (Test Recommends) prereq

--- lib/Number/Phone/Formatter/EPP.pm
+++ lib/Number/Phone/Formatter/EPP.pm
@@ -7,8 +7,10 @@ use warnings;
 
 =method format
 
-This is the only method.  It takes an L<E.123|http://www.itu.int/rec/T-REC-E.123> international format string as its
-only argument and reformats it in EPP (L<RFC 5733|https://tools.ietf.org/html/rfc5733>) format.  For example:
+This is the only method.  It takes an
+L<E.123|http://www.itu.int/rec/T-REC-E.123> international format string as its
+only argument and reformats it in EPP
+(L<RFC 5733|https://tools.ietf.org/html/rfc5733>) format.  For example:
 
  +44 20 8771 2924 -> +44.2087712924
  +1 212 334 0611  -> +1.2123340611
@@ -28,10 +30,28 @@ sub format {
 
 1;
 
+=head1 SYNOPSIS
+
+ # Option 1: use with Number::Phone
+ #
+ # prints: +44.2087712924
+ #
+ my $number = Number::Phone->new('+44 20 8771 2924');
+ print $number->format_using('EPP');
+
+ # Option 2: use standaline, without Number::Phone
+ #
+ # prints: +44.2087712924
+ #
+ print Number::Phone::Formatter::EPP->format('+44 20 8771 2924');
+
 =head1 DESCRIPTION
 
-This is a formatter that will format an L<E.123|http://www.itu.int/rec/T-REC-E.123> formatted number (from
-L<Number::Phone>) as an EPP phone number.
+This is a formatter that will format an
+L<E.123|http://www.itu.int/rec/T-REC-E.123> formatted number (from
+L<Number::Phone>) as an EPP phone number.  This can be used with
+L<Number::Phone>, or, as a standalone module if you do not need the features of
+L<Number::Phone>.
 
 =head1 SEE ALSO
 
